ae3ecb11d7226640817857959757e185b262cc23
nicooniate <nicooniate02@gmail.com>
2025-07-15 17:12:52 -0300

    Add Standard Line

---
+++ proyects/03-mouse-follower/src/App.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    console.log("Efecto", enabled);
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
+
+    // Cleanup
+    // -> Cuando el componente se desmonta o cuando 'enabled' cambia
+    return () => {
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "rgba(44, 46, 49, 0.53)", // azul claro con transparencia
+          borderRadius: "50%",
+          border: "1px solid #fff", // borde blanco
+          pointerEvents: "none",
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+          transition: "transform 0.05s ease-out", // movimiento más fluido
+        }}
+      />
+
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Desactivar" : "Activar"} seguir puntero
+      </button>
+    </>
+  );
+};
+
+function App() {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
+  );
+}
+
+export default App;
